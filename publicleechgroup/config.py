552fcfd35e4f739bb9440fe802a99498354f95ae
abhishekhbhootwala <88439521+abhishekhbhootwala@users.noreply.github.com>
2022-04-15 13:01:25 +0600

    Create config.py

---
+++ publicleechgroup/config.py
@@ -0,0 +1,10 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1852136534:AAFg1RIQDrzqUI_piSSrB3u1pYp2cix-7Gk"
+  APP_ID = 6134343
+  API_HASH = "344493a60221b6483e47b00ff1461708"
+  AUTH_CHANNEL = [-1001531109672]
+  OWNER_ID = 548699498
+  DESTINATION_FOLDER = "Download"
+  RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.A0ARrdaM_PgpaLewiUTDGDNi2tERuyUcSvPBk1V-TmCWCnV8QFeYxULsrlRXcV_JvNhhDTMwZprdPr97sskR10YBw715h0k-UzwAb3s0XtzaqiKgsGgnz-T-hv1AeRG0tJwgrcHNb8D7shKCMgC_V27A22dTRf","token_type":"Bearer","refresh_token":"1//0guyXQFFb3u_rCgYIARAAGBASNwF-L9Ir3fmIUyW8RNdQSmWCpf1M0kGBW6wTkKFQLZrCyZFSN2ByJGkyIRR654OfmuJ8mmvuQlw","expiry":"2022-04-15T13:53:20.1349053+06:00"}"""
